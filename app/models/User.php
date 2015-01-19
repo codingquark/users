a5f5874c37340b69d97a0177abbf0519a0464dda
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2015-01-19 18:46:16 +0530

    Role management methods
    Added helper methods to manage roles.

--- app/models/User.php
+++ app/models/User.php
@@ -11,4 +11,52 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 class User extends Eloquent implements UserInterface, RemindableInterface{
     use ConfideUser;
     use HasRole;
+
+    public function saveRoles($inputRoles)
+    {
+        if(! empty($inputRoles)) {
+            $this->roles()->sync($inputRoles);
+        } else {
+            $this->roles()->detach();
+        }
+    }
+
+    public function currentRoleIds()
+    {
+        $roles = $this->roles;
+        $roleIds = false;
+        if( !empty( $roles ) ) {
+            $roleIds = array();
+            foreach( $roles as &$role )
+            {
+                $roleIds[] = $role->id;
+            }
+        }
+        return $roleIds;
+    }
+
+    public static function checkAuthAndRedirect($redirect, $ifValid=false)
+    {
+        // Get the user information
+        $user = Auth::user();
+        $redirectTo = false;
+
+        if(empty($user->id) && ! $ifValid) // Not logged in redirect, set session.
+        {
+            Session::put('loginRedirect', $redirect);
+            $redirectTo = Redirect::to('user/login')
+                ->with( 'notice', Lang::get('user/user.login_first') );
+        }
+        elseif(!empty($user->id) && $ifValid) // Valid user, we want to redirect.
+        {
+            $redirectTo = Redirect::to($redirect);
+        }
+
+        return array($user, $redirectTo);
+    }
+
+    public function currentUser()
+    {
+        return (new Confide(new ConfideEloquentRepository()))->user();
+    }
 }
