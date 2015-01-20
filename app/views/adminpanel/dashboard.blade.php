66cf8dcabedef1c48193257d9bd9d1b11b126868
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2015-01-20 12:57:07 +0530

    Updated Admin dashboard
    Displaying list of all users to the Admin

--- app/views/adminpanel/dashboard.blade.php
+++ app/views/adminpanel/dashboard.blade.php
@@ -1,5 +1,50 @@
 @extends('master')
 
+@section('head')
+
+	<link rel="stylesheet" type="text/css" href="css/admindash.css">
+
+
+@stop
+
+
 @section('content')
+
+	<div class="container">
+		<div class="jumbotron">
+			<h2>List of Users:</h2>
+			<table class="table">
+		        <thead>
+		            <tr>
+		                <th>No.</th>
+		                <th>Name</th>
+		                <th>Email</th>
+		                <th>Role</th>
+		            </tr>
+		        </thead>
+		        <tbody>
+		            
+		                @foreach ($users as $user)
+			                <tr>
+			                	<td>{{ $user->id }}</td>
+		    					<td>{{ $user->username }}</td>
+		    					<td>{{ $user->email }}</td>
+		    					<td>
+		    						@foreach( Confide::user()->roles as $role)
+							    		{{ $role->name }}
+							    	@endforeach
+		    					</td>
+		    				</tr>
+	    				@endforeach
+		            
+		        </tbody>
+	    	</table>
+
+	    	{{-- Confide::user()->roles --}}
+	    	<a href="/users/logout">Logout</a>
+		</div>
+	</div>
+
+	
 
 @stop
