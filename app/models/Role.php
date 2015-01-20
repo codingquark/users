39ab751a84e1af6b67764ed7fef8c4656dd6947c
Dhavan Vaidya <dhavan.vaidya6@gmail.com>
2015-01-20 15:50:03 +0530

    Removed Unused Methods
    Removed unused methods from Role, User models

--- app/models/Role.php
+++ app/models/Role.php
@@ -4,15 +4,5 @@ use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
 {
-	public function validateRoles( array $roles )
-    {
-        $user = Confide::user();
-        $roleValidation = new stdClass();
-        foreach( $roles as $role )
-        {
-            // Make sure theres a valid user, then check role.
-            $roleValidation->$role = ( empty($user) ? false : $user->hasRole($role) );
-        }
-        return $roleValidation;
-    }
+    
 }
